82c0fcbb36e85312fe4cd35c5721ab45ee3ec8ee
guninkakar03 <guninkakar03@gmail.com>
2023-06-02 20:29:20 +0530

    completed the script for tables of database, need to test and make a main for the same

--- Scripts/databaseTracker.py
+++ Scripts/databaseTracker.py
@@ -1,16 +1,17 @@
 import pymysql
 
-
-connection = pymysql.connect(
-    host='localhost',  # Hostname
-    user='root',  # MySQL username
-    password='utm@GDSC',  # MySQL password
-    database='testDatabase'  # Database name
-)
+"""
+This script will help to create all the tables needed to 
+store the data for the MyAcademia application.
+"""
 
 
+# making connection to the database
+connection = pymysql.connect(host='localhost', user='root', password='utm@GDSC',
+                             database='testDatabase')
+
+# this helps to add quires
 cursor = connection.cursor()
-
 
 # create courseDescription table
 create_courseDescription = '''CREATE TABLE IF NOT EXISTS courseDescription (
@@ -46,17 +47,64 @@ create_requirements = '''CREATE TABLE IF NOT EXISTS requirements (
     program_ID       INT           NOT NULL,
     CONSTRAINT requirements_programs_program_ID_fk
         FOREIGN KEY (program_ID) REFERENCES programs (program_ID)
+); '''
+
+# create users table
+create_users = '''CREATE TABLE IF NOT EXISTS myAcademia_users (
+    `UofT Email` VARCHAR(25)  NOT NULL PRIMARY KEY,
+    Name         VARCHAR(50)  NOT NULL,
+    userName     VARCHAR(8)   NOT NULL,
+    Password     VARCHAR(256) NOT NULL,
+    CONSTRAINT myAcademia_users_pk UNIQUE (userName)
+); '''
+
+# create professors table
+create_professors = ''' CREATE TABLE IF NOT EXISTS professor (
+    prof_ID         INT          NOT NULL PRIMARY KEY,
+    professor_name  VARCHAR(400) NOT NULL,
+    department      VARCHAR(100) NULL
+); '''
+
+# create onlineReviews Table
+create_onlineReviews = '''CREATE TABLE IF NOT EXISTS onlineReviews (
+    `review ID`                VARCHAR(100)  NOT NULL PRIMARY KEY,
+    `sentiment analysis value` INT           NULL,
+    reviews                    VARCHAR(2500) NULL,
+    `course code`              VARCHAR(100)  NOT NULL,
+    CONSTRAINT `onlineReviews_courses_course_code_fk`
+        FOREIGN KEY (`course code`) REFERENCES courses (course_code)
 );
-'''
+ '''
 
+# create userReviews Table
+create_userReviews = '''CREATE TABLE IF NOT EXISTS userReviews (
+    `review ID`     VARCHAR(100)  NOT NULL,
+    review          VARCHAR(1000) NULL,
+    rating          INT           NULL,
+    `course code`   VARCHAR(100)  NOT NULL,
+    userName        VARCHAR(100)  NOT NULL,
+    professor       VARCHAR(100)  NULL,
+    `professor ID`  INT           NOT NULL,
+    CONSTRAINT course_code
+        FOREIGN KEY (`course code`) REFERENCES courses (course_code),
+    CONSTRAINT userName
+        FOREIGN KEY (userName) REFERENCES myAcademia_users (userName),
+    CONSTRAINT userReviews_professor_prof_ID_fk
+        FOREIGN KEY (`professor ID`) REFERENCES professor (prof_ID)
+);
+ '''
 
-# running the queries
+# running the queries!!
 cursor.execute(create_courseDescription)
 cursor.execute(create_courses)
 cursor.execute(create_programs)
 cursor.execute(create_requirements)
+cursor.execute(create_users)
+cursor.execute(create_professors)
+cursor.execute(create_onlineReviews)
+cursor.execute(create_userReviews)
 
-# Commit the changes: This line essenitaly saves the changes and adds to
+# Commit the changes: This line essentially saves the changes and adds to
 # the database, similar to git commit :)
 connection.commit()
 
---
+++ Scripts/setupDatabase.py
@@ -0,0 +1,4 @@
+"""
+This script will be used to setup the database for the MyAcademia application
+especially when any changes are made to keep the team members updated.
+"""
