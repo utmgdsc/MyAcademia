418a3ebd39d84a826cf0933c0eddc1296b6c3cba
AkhilChoraria <chorariaakhil03@gmail.com>
2023-07-11 10:14:02 -0400

    Working on extracting the degree-exp requirements and added a file to scarpe the professor and their department from academic calendar

--- Backend/courses/views.py
+++ Backend/courses/views.py
@@ -4,15 +4,17 @@ from .serializers import CourseSerializer
 from .models import Course
 from django.http import request
 
+
 # To view courses in the backend
 class CourseView(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+
 # Create your views here.
 
 # Extracts information from the request and returns course objects which match the criteria
-def courseSearch(request : request):
+def courseSearch(request: request):
     if request.METHOD == 'POST':
         course_code = request.data['course_code']
         pre_req = request.data['pre_req']
@@ -21,11 +23,9 @@ def courseSearch(request : request):
         return filterCourses(course_code, pre_req, distribution, program_area)
 
 
-def filterCourses(course_code: str, pre_req: str, distribution:str, program_area: str) -> Course.objects:
-    if course_code != None or course_code != "": # Course code provided so return course with the course code
-        return Course.objects.filter(course_code = course_code)
+def filterCourses(course_code: str, pre_req: str, distribution: str, program_area: str) -> Course.objects:
+    if course_code != None or course_code != "":  # Course code provided so return course with the course code
+        return Course.objects.filter(course_code=course_code)
     # Filter courses based on the criteria provided. Program area is not implemented yet
-    return Course.objects.all().filter(course_code = course_code).filter(pre_req__icontains = pre_req)\
-        .filter(distribution = distribution)#.filter(program_area = program_area)
-    
-    
+    return Course.objects.all().filter(course_code=course_code).filter(pre_req__icontains=pre_req) \
+        .filter(distribution=distribution)  # .filter(program_area = program_area)
