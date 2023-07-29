2b7f6ca490cddbc1e1ce3d9c9e30f0c7f349d22b
adityaharish <adityaharish218@gmail.com>
2023-07-29 17:24:42 -0400

    Changed createReviewView

--- Backend/reviews/views.py
+++ Backend/reviews/views.py
@@ -7,6 +7,9 @@ from courses.models import Course
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+import jwt
+import Backend.settings as settings
+from rest_framework_simplejwt.authentication import JWTAuthentication
 # To view User Reviews in the backend
 class UserReviewView(viewsets.ModelViewSet):
     serializer_class = UserReviewSerializer
@@ -24,41 +27,60 @@ class ProfessorView(viewsets.ModelViewSet):
 
 # Create your views here.
 
-# Class that handles the creation of a new user review
+# Class that handles the creation of a new user review. Doesn't work perfectly yet as authentication is not working
 class CreateReviewView(APIView):
     permission_classes = [permissions.IsAuthenticated] # Only authenticated users can access this endpoint
     #permission_classes = [permissions.AllowAny] # Allow any user to access this endpoint. Only for testing
+    authentication_classes = [JWTAuthentication]
     http_method_names = ['post']
     # A post method to handle the request and create a new user review
     def post(self, request, *args, **kwargs):
-            # Extract the review data from request
-        username = request.data['username']
+        # Extract the user information from the token
+        authorization_header = request.headers.get('Authorization')
+        if not authorization_header or not authorization_header.startswith('Bearer '):
+            return Response({f'Error, invalid arguments here, {authorization_header}'}, status=status.HTTP_400_BAD_REQUEST)
+        token = authorization_header.split(' ')[1]
+        try:
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
+            username = decoded_token['username']
+        except jwt.ExpiredSignatureError:
+            return Response({'Error, token expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.InvalidTokenError:
+            return Response({'Error, invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if token is not None:
+            return Response({f'Token got : {token}'}, status=status.HTTP_400_BAD_REQUEST)
+        # Extract the review data from request
         course_code = request.data['course_code']
         professor_name = request.data['professor_name']
         department = request.data['department']
         rating = request.data['rating']
         review = request.data['review']
+        anonymous = request.data.get('anonymous', False)
         if not username or not course_code or not rating or not review or not professor_name or not department:
             return Response({'Error, invalid arguments'}, status=status.HTTP_400_BAD_REQUEST)
         # Check if the user exists and add if not AnonymousUser
         userReview = UserReview()
         user = User.objects.get(username=username)
-        if not user and username != 'AnonymousUser':
+        if not user:
             return Response({'Error, user does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-        if username != 'AnonymousUser':
-            userReview.userName = user
+        if  not anonymous :
+            userReview.username = user
+
         # Check if the course exists
         course = Course.objects.get(course_code=course_code)
         if not course:
             return Response({'Error, course does not exist'}, status=status.HTTP_400_BAD_REQUEST)
         userReview.course = course
+
         # Check if the professor exists
         professor = Professor.objects.filter(professor_name=professor_name).filter(department=department)
         if len(professor) != 0:
-            userReview.Professor = professor[0] # We may need to change this later. Currently it uniquely identifies a professor based on name and department.
+            userReview.Professor = professor.first() # We may need to change this later. Currently it uniquely identifies a professor based on name and department.
             userReview.Professor.add_course(course)  
         if len(professor) == 0 and professor_name != 'NoProfessor':
             return Response({'Error, professor does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+        
         # Add rating and review. Store the review in the database
         userReview.rating = rating
         userReview.review = review
